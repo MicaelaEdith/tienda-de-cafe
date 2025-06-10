b972ea8464cb5232982a56690d127ff939f38d00
micaelaedith <romeromicaelaedith@gmail.com>
2025-06-09 21:01:43 -0300

    tutorial 1 ok

--- js/tutoriales.js
+++ js/tutoriales.js
@@ -1,5 +1,9 @@
 window.addEventListener("scroll", function () {
-const parallax = document.getElementById("parallax");
-const offset = window.scrollY;
-parallax.style.backgroundPositionY = offset * -0.8 + "px";
+  const parallaxSections = document.querySelectorAll(".parallax-header");
+
+  parallaxSections.forEach(section => {
+    const rect = section.getBoundingClientRect();
+    const offset = rect.top;
+    section.style.backgroundPositionY = offset * 0.3 + "px";
+  });
 });
